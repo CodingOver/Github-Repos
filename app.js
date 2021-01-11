e44af591c1298a642c2235e021cc145e99853b4c
Ayoub Fellat <paypal1business.over@gmail.com>
2021-01-11 12:18:46 -0800

    update function and clean code

--- app.js
+++ app.js
@@ -1,79 +1,42 @@
 // Main Variables
-let input = document.querySelector('.get-repos input');
-let getButton = document.querySelector('.get-button');
-let reposData = document.querySelector('.show-data');
+function $(id) {
+    return document.getElementById(id);
+}
 
-getButton.onclick = function () {
-    getRepos();
+let button = $('button');
+let reposData = $('result');
+
+button.onclick = async function () {
+    const repos = await fechRepos();
+    clear();
+    printRepos(repos);
 };
 
-// Get Repos Function 
-function getRepos() { // If Value Is Empty
+function clear() {
+    reposData.innerHTML = '';
+}
 
-    if (input.value == '') {
+function printNoResult(message = 'Please Write Github UserName') {
+    reposData.innerHTML = `<span class='empty'>${message}<span>`
+}
 
-        reposData.innerHTML = "<span class='empty'>Please Write Github UserName<span>";
+function card({name, html_url: htmlUrl, stargazers_count: stargazersCount}) {
+    return `<div class="repo-box">${name}
+    <a href=${htmlUrl} target="_blank">Visit</a>
+    <span>Stars ${stargazersCount}</span>
+    </div>`
+}
 
-    } else { // Fetch API Github 
+function printRepos(repos) {
+    if (repos instanceof Array) 
+        reposData.innerHTML = repos.map(card).join('');
+    else printNoResult(repos.message);
+}
 
-        fetch(`https://api.github.com/users/${input.value}/repos`)
-            .then(response => response.json())
-            .then(repsitories => {
-
-                // Empty The Container
-                reposData.innerHTML = '';
-
-                // Loop on Repsitories
-                repsitories.forEach(repo => {
-
-                    // Create The Main Div 
-                    let mainDiv = document.createElement('div');
-                    
-                    // Add Class To Main Div 
-                    mainDiv.className = 'repo-box';
-
-                    // Create Repo Name Text
-                    let repoName = document.createTextNode(repo.name);
-
-                    // append The Repo Name into The Main Div
-                    mainDiv.appendChild(repoName);
-
-                     // Create Repo URL Anchor
-                     let theUrl = document.createElement('a');
-                     
-                     // Create repo text 
-                     let theUrlText = document.createTextNode('Visit');
-
-                     // Append The URL Text into Anchor Text
-                     theUrl.appendChild(theUrlText);
-
-                     // Add Href Attributes To Repo Url Anchor
-                     theUrl.href = `https://github.com/${input.value}/${repo.name}`;
-
-                     // Set Target Blanks To Repo Url Anchor
-                     theUrl.setAttribute('target', '_blank');
-
-                     // Append Url Anchor To Main Div
-                     mainDiv.append(theUrl);
-                     // Append Url Anchor To Container
-                     reposData.appendChild(mainDiv);
-
-                     // Create Star count Span
-                     let starsSpan = document.createElement('span');
-
-                     // Create The Stars Count Text
-                     let starsText = document.createTextNode(`Stars ${repo.stargazers_count}`);
-
-                     // Add Stars Count Text To Stars Count Span
-                     starsSpan.appendChild(starsText);
-                     
-                     // Append The Stars Count Span To Main Div
-                     mainDiv.appendChild(starsSpan);
-
-                     // append The main Div To Container
-                     reposData.appendChild(mainDiv);
-                });
-            });
-    }
-
+function fechRepos() {
+    const query = $('search');
+    if(!query) return printNoResult();
+    return fetch(`https://api.github.com/users/${query.value}/repos`)
+    .then(response => response.json())
+    .then(repsitories => repsitories)
 }
